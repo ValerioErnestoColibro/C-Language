1e41dee3f2c79f7605f4b261321a8c1a702397c9
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-03-07 13:47:12 +0100

    New file

---
+++ Numero+Grande.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main() {
+	
+int numero1,numero2;
+
+printf("Inserisci il primo numero: ");
+scanf("%d",&numero1);
+
+printf("Inserisci il secondo numero: ");
+scanf("%d",&numero2);
+
+if(numero1>numero1){
+	printf("Il primo numero \x8A quello maggiore");
+} else {
+	printf("Il secondo numero \x8A quello maggiore");
+}
+
+}
