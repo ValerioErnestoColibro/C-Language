f788cfc4378e243aa905e62dc22f6a662d0fcd88
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-03-22 11:32:06 +0100

    New file

---
+++ FunzioniSomma.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int somma(int a, int b);
+
+int main() { 
+	printf("%d",somma(1,2));
+}
+
+int somma(int a, int b){
+	return a+b;
+}
